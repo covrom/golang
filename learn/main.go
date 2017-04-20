c7880896a2ad3fa0ba523080bdc9f0ea1f9ac0a5
Roman TSovanyan <covrom@gmail.com>
2017-04-20 21:59:42 +0300

    dup1

--- learn/main.go
+++ learn/main.go
@@ -2,60 +2,19 @@ package main
 import (
 	"fmt"
 	"os"
-	//"strings"
+	"bufio"
 )
 
-//коммент
 func main(){
-	// fmt.Println("1+1=",1+1)
-	// fmt.Println(len("Привет всем"))
-	// fmt.Println("Привет всем"[12]) //берет побайтно, юникодовые символы считает по два байта, остальные по одному
-	// fmt.Println("Привет"+"всем")
-	// fmt.Println(true && true)
-	// fmt.Println(true && false)
-	// fmt.Println(true || false)
-	// fmt.Println(true || true)
-	// fmt.Println(!true)
-
-	// var x string 
-	// var x = "Привет всем"
-
-	// x := "Привет всем"
-	// x = x+"!"
-	// fmt.Println(x)
-
-	// fmt.Print("Дай число:")
-	// var num float64
-	// fmt.Scanf("%f",&num)
-	// dbl := num*num
-	// fmt.Println(dbl)
-
-	// fmt.Println(`ikgh
-	// piohoijh
-	// ;ih
-	// `)
-
-	// i:=1
-	// for i<=10{
-	// 	if i%2==0{
-	// 		fmt.Println(i,"четное")
-	// 	}else{
-	// 		fmt.Println(i,"нечетное")
-	// 	}
-		
-	// 	i++
-	// }
-
-	// elems := map[string]string{}
-
-	//fmt.Println(os.Args[0])
-	//fmt.Println(strings.Join(os.Args[1:]," "))
-
-	s,sep:="",""
-	for _,arg:=range os.Args[:]{
-		s+=sep+arg
-		sep=" "
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan(){
+		counts[input.Text()]++
 	}
-	fmt.Println(s)
+	for line,n := range counts{
+		if n>1{
+			fmt.Printf("%d\t%s\n",n,line)
+		}
+	}
 
 }
