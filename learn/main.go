72ecf78f24321ef9e8256ddff12511ca76b37fa5
Roman TSovanyan <covrom@gmail.com>
2017-04-20 23:24:48 +0300

    dup3

--- learn/main.go
+++ learn/main.go
@@ -1,26 +1,24 @@
 package main
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
-	"bufio"
+	"strings"
 )
 
 func main(){
 	counts:=make(map[string]int)
-	files:=os.Args[1:]
-	if len(files)==0{
-		countLines(os.Stdin, counts)
-	}else{
-		for _,arg:=range files{
-			f,err:=os.Open(arg)
-			if err!=nil{
-				fmt.Fprintf(os.Stderr,"dup2: %v\n",err)
-				continue
-			}
-			countLines(f,counts)
-			f.Close()
+	for _,filename:=range os.Args[1:]{
+		data,err:=ioutil.ReadFile(filename)
+		if err!=nil{
+			fmt.Fprintf(os.Stderr,"dup3: %v\n",err)
+			continue
+		}
+		for _,line:=range strings.Split(string(data),"\n"){
+			counts[line]++
 		}
 	}
+	
 	for line,n:=range counts{
 		if n>1{
 			fmt.Printf("%d\t%s\n",n,line)
@@ -28,10 +26,3 @@ func main(){
 	}
 }
 
-func countLines(f *os.File, counts map[string]int){
-	input:=bufio.NewScanner(f)
-	for input.Scan(){
-		counts[input.Text()]++
-	}
-	//игнорируем ошибки
-}
