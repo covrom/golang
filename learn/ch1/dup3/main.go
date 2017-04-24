7265d6e0314667f0e76de8512ac0eb22f9c24e38
Roman TSovanyan <covrom@gmail.com>
2017-04-24 21:58:03 +0300

    from book

--- learn/ch1/dup3/main.go
+++ learn/ch1/dup3/main.go
@@ -1,4 +1,14 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 12.
+
+//!+
+
+// Dup3 prints the count and text of lines that
+// appear more than once in the named input files.
 package main
+
 import (
 	"fmt"
 	"io/ioutil"
@@ -6,21 +16,23 @@ import (
 	"strings"
 )
 
-func main(){
-	counts:=make(map[string]int)
-	for _,filename:=range os.Args[1:]{
-		data,err:=ioutil.ReadFile(filename)
-		if err!=nil{
-			fmt.Fprintf(os.Stderr,"dup3: %v\n",err)
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
 			continue
 		}
-		for _,line:=range strings.Split(string(data),"\n"){
+		for _, line := range strings.Split(string(data), "\n") {
 			counts[line]++
-			if counts[line]>1{
-				fmt.Println(filename)
-				break
-			}
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
 }
 
+//!-
