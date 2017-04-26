ca235d14bd76136acbb9a0dc583ff8f34ce53dcf
Roman TSovanyan <covrom@gmail.com>
2017-04-26 22:54:54 +0300

    ex 1.4

--- learn/ch1/dup2/main.go
+++ learn/ch1/dup2/main.go
@@ -30,17 +30,23 @@ func main() {
 			f.Close()
 		}
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
+	// for line, n := range counts {
+	// 	if n > 1 {
+	// 		fmt.Printf("%d\t%s\n", n, line)
+	// 	}
+	// }
 }
 
 func countLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
+	hasdbl:=false
 	for input.Scan() {
-		counts[input.Text()]++
+		itx:=input.Text()
+		counts[itx]++
+		hasdbl = hasdbl || (counts[itx] > 1)
+	}
+	if hasdbl{
+		fmt.Printf("Есть дубли в файле %s\n", f.Name())
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
