a8937ea287b560ea618e577a1a5242e3714a4c08
Roman TSovanyan <covrom@gmail.com>
2017-05-01 17:33:24 +0300

    ex 3.3, 3.4

--- learn/ch3/surface/main.go
+++ learn/ch3/surface/main.go
@@ -10,6 +10,10 @@ package main
 import (
 	"fmt"
 	"math"
+	"io"
+	"os"
+	"net/http"
+	"log"
 )
 
 const (
@@ -24,34 +28,62 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
 func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		//!+http
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("ContentType", "image/svg")
+			surface(w)
+		}
+		http.HandleFunc("/", handler)
+		//!-http
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	//!+main
+	surface(os.Stdout)
+
+}
+
+func surface(w io.Writer){
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			ax, ay, _ := corner(i+1, j)
+			bx, by,clr := corner(i, j)
+			cx, cy, _ := corner(i, j+1)
+			dx, dy, _ := corner(i+1, j+1)
+			fmt.Fprintf(w, "<polygon points='%g,%g %g,%g %g,%g %g,%g' style='fill:%s'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy, clr)
 		}
 	}
-	fmt.Println("</svg>")
+	fmt.Fprintln(w, "</svg>")
 }
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j int) (float64, float64, string) {
 	// Find point (x,y) at corner of cell (i,j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
 	// Compute surface height z.
 	z := f(x, y)
+	cBlue := byte((0.5-z/2)*255)
+	cRed := byte((0.5+z/2)*255)
+	if z>=1.0{
+		cBlue = 0
+		cRed = 255
+	}
+	if z<=-1.0{
+		cBlue = 255
+		cRed = 0
+	}
+	rgb := fmt.Sprintf("#%x00%x",cRed,cBlue)
 
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy
+	return sx, sy, rgb
 }
 
 func f(x, y float64) float64 {
