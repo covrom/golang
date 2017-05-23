b1a902dd849dc082ade7ec5d4f667726764d0e9e
Roman TSovanyan <covrom@gmail.com>
2017-05-23 09:07:41 +0300

    small changes

--- mapgrow/main.go
+++ mapgrow/main.go
@@ -10,26 +10,26 @@ func main() {
 
 	mymap := make(myMap, 1)
 	mymap["firstKey"] = "firstValue"
-	fmt.Printf("Init: Address = %p Len = %d\n", &mymap, len(mymap))
+	fmt.Printf("Init nop: Address = %p Len = %d\n", &mymap, len(mymap))
 	mymap.grow()
-	fmt.Printf("Growed: Address = %p Len = %d\n", &mymap, len(mymap))
+	fmt.Printf("Growed nop: Address = %p Len = %d\n", &mymap, len(mymap))
 
 	mymap = make(myMap, 1)
 	mymap["firstKey"] = "firstValue"
-	fmt.Printf("Init: Address = %p Len = %d\n", &mymap, len(mymap))
+	fmt.Printf("Init p: Address = %p Len = %d\n", &mymap, len(mymap))
 	(&mymap).growp()
-	fmt.Printf("Growed: Address = %p Len = %d\n", &mymap, len(mymap))
+	fmt.Printf("Growed p: Address = %p Len = %d\n", &mymap, len(mymap))
 
 }
 
 func (m myMap) grow() {
-	for i := 1; i < 100; i++ {
+	for i := 1; i < 1000000; i++ {
 		m[fmt.Sprintf("nopAddKey%d", i)] = fmt.Sprintf("%d", i)
 	}
 }
 
 func (m *myMap) growp() {
-	for i := 1; i < 100; i++ {
+	for i := 1; i < 1000000; i++ {
 		(*m)[fmt.Sprintf("pAddKey%d", i)] = fmt.Sprintf("%d", i)
 	}
 }
